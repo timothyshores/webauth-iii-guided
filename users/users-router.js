bce87b4e8920bcdd16a5467dae8c6269b894e106
Timothy Shores <timothymshores@gmail.com>
2019-05-08 12:51:14 -0500

    Create middleware checkRole(role) in users/users-router.js

--- users/users-router.js
+++ users/users-router.js
@@ -3,12 +3,18 @@ const router = require('express').Router();
 const Users = require('./users-model.js');
 const restricted = require('../auth/restricted-middleware.js');
 
-router.get('/', restricted, (req, res) => {
+router.get('/', restricted, checkRole('sales') (req, res) => {
     Users.find()
         .then(users => {
             res.json(users);
         })
         .catch(err => res.send(err));
 });
+
+function checkRole(role) {
+    return function (req, res, next) {
+        
+    }
+}
 
 module.exports = router;
