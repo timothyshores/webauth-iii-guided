05595cc1bf3dbf10ba7896cf6b0cb9a71876ad03
Timothy Shores <timothymshores@gmail.com>
2019-05-08 12:40:43 -0500

    Remove decoded token in JSON from users/users-router.js

--- users/users-router.js
+++ users/users-router.js
@@ -4,11 +4,11 @@ const Users = require('./users-model.js');
 const restricted = require('../auth/restricted-middleware.js');
 
 router.get('/', restricted, (req, res) => {
-  Users.find()
-    .then(users => {
-        res.json({ users, decodedToken: req.decodedToken });
-    })
-    .catch(err => res.send(err));
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
 });
 
 module.exports = router;
