8f55f56c7bc493d5e7d54efef057dbedbd1c00f9
Timothy Shores <timothymshores@gmail.com>
2019-05-08 12:36:50 -0500

    Add req.decodedToken to users-router and auth/restricted-middleware

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -6,8 +6,10 @@ module.exports = (req, res, next) => {
     jwt.verify(token, secret, (err, decodedToken) => {
         if (err) {
             // token is invalid, expired or modified
+            res.status(401).json({error: "Invalid token"})
         } else {
             // token is valid
+            next();
         }
     })
 
